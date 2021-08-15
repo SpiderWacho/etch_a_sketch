a66e3057243dbed847cd8176a512d591e50fb7b3
Spider <gastonvecch@gmail.com>
2021-08-15 20:37:23 -0300

    Made createBoard function and made funcional the clear button

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch_a_sketch
+
+A project where i try to create a etch-a-sketch page, in this project i would try to apply DOM manipulation.
---
+++ script.js
@@ -0,0 +1,38 @@
+const boardHolder = document.querySelector("#board")
+
+function createBoard(size) {
+for (let i = 0; i < size; i++){
+    for (let j = 0; j < size; j++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("boardDiv")
+        boardHolder.appendChild(newDiv);
+    }
+}
+boardTiles = document.querySelectorAll(".boardDiv");
+boardTiles.forEach(boardTile => { boardTile.addEventListener("mouseenter", changeColor)     
+});
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+//Initial size
+let size = 16;
+createBoard(size);
+
+
+
+ const clearButton = document.querySelector("#clear");
+ clearButton.addEventListener("click", function clearGrid(){
+    boardTiles.forEach(boardTile => boardTile.style.backgroundColor = "transparent");
+    let gridSize = 0;
+    while (gridSize === 0 || gridSize >100 ) {
+        gridSize = prompt("Enter size of grid");
+    }
+    createBoard(gridSize);
+    const board = document.querySelector("#board");    
+    board.setAttribute('style',
+    'grid-template-columns: repeat(' + gridSize + ', auto)',
+    'grid-template-rows: repeat(' + gridSize + ', auto)');
+ });
